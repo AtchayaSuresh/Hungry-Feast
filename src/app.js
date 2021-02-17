530f9fcb0148614cf18c0323095325bf87ca7615
Chams <atchayasuresh17@gmail.com>
2021-02-17 21:21:31 +0530

    1721994

--- src/app.js
+++ src/app.js
@@ -1,42 +1,58 @@
-const canvas=document.getElementById('canvas')
-const ctx=canvas.getContext('2d');
+const ocean=document.getElementById('underwater')
+const oceanSpace=ocean.getContext('2d');
 
-canvas.width=1510;
-canvas.height=710;
+ocean.width=1510;
+ocean.height=710;
 
-let canvasPosition=canvas.getBoundingClientRect();
+let oceanPosition=ocean.getBoundingClientRect();
 
 let gameFrame=0;
 let score=0;
+let seconds=60;
+if(sessionStorage.getItem("highScore")==null)
+sessionStorage.setItem("highScore", 0);
 
-ctx.font='50px TimesNewRoman';
+oceanSpace.font='50px TimesNewRoman';
 
 const mouse= {
-    x:canvas.width/2,
-    y:canvas.height/2,
+    x:ocean.width/2,
+    y:ocean.height/2,
     click:false,
 }
 
-canvas.addEventListener("mousedown",function(event){
+ocean.addEventListener("mousedown",function(event){
     mouse.click=true;
-    mouse.x=event.x-canvasPosition.left;
-    mouse.y=event.y-canvasPosition.top;
+    mouse.x=event.x-oceanPosition.left;
+    mouse.y=event.y-oceanPosition.top;
 });
 
-canvas.addEventListener("mouseup",function(event){
+ocean.addEventListener("mouseup",function(event){
     mouse.click=false;
 });
 
+const playerFishLeft=new Image();
+playerFishLeft.src='images/playerFishLeft.png';
+const playerFishRight=new Image();
+playerFishRight.src='images/playerFishRight.png';
+
+const preyFish=new Image();
+preyFish.src='images/preyFish.png';
+
+
 class PlayerFish{
     constructor(){
-        this.x=canvas.width/2;
-        this.y=canvas.height/2;
+        this.x=ocean.width/2;
+        this.y=ocean.height/2;
         this.radius=50;
-        }
+        this.angle=0;
+    }
 
     update(){
         const currentX=this.x-mouse.x;
         const currentY=this.y-mouse.y;
+
+        //update angle of the fish
+        this.angle=Math.atan2(currentY,currentX);
 
         if(mouse.x!=this.x){
             this.x-=currentX/20;
@@ -49,24 +65,32 @@ class PlayerFish{
 
     draw(){
         if(mouse.click){
-            ctx.moveTo(this.x,this.y);
+            oceanSpace.moveTo(this.x,this.y);
         }
-        ctx.fillStyle="green";
-        ctx.beginPath();
-        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2);
-        ctx.fill();
-        ctx.closePath();
+
+        //to make the fish turn towards the prey
+        oceanSpace.save();
+        oceanSpace.translate(this.x,this.y);
+        oceanSpace.rotate(this.angle);
+
+        if(this.x>=mouse.x)
+        oceanSpace.drawImage(playerFishLeft,-80,-80);
+        else
+        oceanSpace.drawImage(playerFishRight,-80,-80);
+
+        //restore previous ocean settings
+        oceanSpace.restore();
     }
 }
 
- const player=new PlayerFish();
+const player=new PlayerFish();
 
 let preyFishes=[];
 
 class PreyFish{
     constructor(){
-        this.y=Math.random()*canvas.height;
-        this.radius=50;
+        this.y=Math.random()*ocean.height;
+        this.radius=70;
         this.x=-this.radius;
         this.speed=Math.random()*5+1;
         this.counted=false;
@@ -75,14 +99,14 @@ class PreyFish{
 
     update(){
         this.x+=this.speed;
+        let currentX=this.x-player.x;
+        let currentY=this.y-player.y;
+        this.distance=Math.sqrt(currentX*currentX + currentY*currentY);
+        
     }
 
     draw(){
-        ctx.fillStyle="blue";
-        ctx.beginPath();
-        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2);
-        ctx.fill();
-        ctx.closePath();
+        oceanSpace.drawImage(preyFish,this.x-80,this.y-70);
     }
 }
 
@@ -94,34 +118,59 @@ function createPreyFish(){
 
     for(let fish=0;fish<preyFishes.length;fish++){
 
-        if(preyFishes[fish].x>canvas.width+this.radius){
-        preyFishes.splice(fish,1);
-        }
+        let preyFish=preyFishes[fish];
+        preyFish.update();
+        preyFish.draw();
         
-        preyFishes[fish].update();
-        preyFishes[fish].draw();
-
-        if(preyFishes[fish].distance < player.radius+preyFishes[fish].radius){
-            this.score++;
+        if(preyFish.x>ocean.width+this.radius){
+            preyFishes.splice(fish,1);
+            fish--;
+            }
+        else if(preyFish.counted==false && preyFish.distance < player.radius + preyFish.radius){
+            score++;
+            preyFish.counted=true;
+            preyFishes.splice(fish,1);
+            fish--;
         }
        
     }
+    
 }
 
-let timer=0;
+let timer = setInterval(function(){
+    if(seconds==0){
+        if(score>sessionStorage.getItem("highScore"))
+        sessionStorage.setItem("highScore",score);
+        
+        document.getElementById("gameSummary").style.display = "block";
+        document.getElementById("gameScore").innerHTML = "YOUR SCORE: "+ score;
+        document.getElementById("highScore").innerHTML = "HIGH SCORE: "+sessionStorage.getItem("highScore");
+    }
+    seconds--;
+}, 1000);
+
 function animate(){
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+    oceanSpace.clearRect(0,0,ocean.width,ocean.height);
     createPreyFish();
     player.update();
     player.draw();
+    //timer();
 
     //to show the score
-    ctx.fillStyle='orange';
-    ctx.fillText("SCORE: "+score,50,70);
+    oceanSpace.fillStyle='white';
+    oceanSpace.fillText("SCORE: "+score,50,70);
 
     //to show the timer
-    ctx.fillStyle='orange';
-    ctx.fillText("TIMER: "+timer,1150,70);
+    oceanSpace.fillStyle='white';
+    oceanSpace.fillText("TIMER: "+seconds,1200,70);
+
+    //to show High Score
+    oceanSpace.fillStyle='white';
+    oceanSpace.fillText("HIGH SCORE: "+sessionStorage.getItem("highScore"),560,70);
+
+    console.log(sessionStorage.getItem("highScore")+score);
+    if(seconds==0)
+    return;
 
     gameFrame++;
     requestAnimationFrame(animate);
